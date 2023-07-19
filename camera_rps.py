5bba2916a4336308ad801c3d67a38cb16d996343
Adam Evans <adamevansjs@gmail.com>
2023-07-19 16:18:23 +0100

    Update manual_rps.py: Tidy up and reorganise structure. Add loop functionality to wait for 3 wins from user or CPU ||| camera_rps.py add game logic from manual_rps.py and use camera input instead of keyboard input

--- camera_rps.py
+++ camera_rps.py
@@ -4,9 +4,10 @@ from keras.models import load_model # machine learning model
 import numpy as np
 import random as rd
 
+model = load_model('keras_model.h5')
+
 def countdown_timer(seconds):
     print("Get ready! Show the camera Rock, Paper or Scissors and press enter when ready. For best results, hold your hand still during the countdown...")
-    enter = input()
     start_time = time.time()
     end_time = start_time + seconds
     countdown = seconds
@@ -22,8 +23,8 @@ def countdown_timer(seconds):
 
 def get_prediction():
     """Returns the output of the Rock Paper Scissors Keras Model"""
-    
-    model = load_model('keras_model.h5') 
+     
+    countdown_timer(3)
     cap = cv2.VideoCapture(1)
     data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)  
 
@@ -35,40 +36,39 @@ def get_prediction():
         data[0] = normalized_image # modifies the np array "data" in place with the new normalized numbers
         prediction = model.predict(data) # stores the prediction based on the normalize numbers in "data"
         cv2.imshow('frame', frame) # imshow() displays the specified frame in a window. The first argument names the window the second specifies the frame
-        countdown_timer(3) 
         final_prediction_index = np.argmax(prediction)
         
         if final_prediction_index == 0:
             print("You chose Nothing")
-            #return "Nothing"
+            return "Nothing"
         elif final_prediction_index == 1:
             print("You chose Rock")
-            #return "Rock"
+            return "Rock"
         elif final_prediction_index == 2:
             print("You chose Paper")
-            #return "Paper"
+            return "Paper"
         elif final_prediction_index == 3:
             print("You chose Scissors")
-            #return "Scissors"
+            return "Scissors"     
 
-get_prediction()
-        
 
-"""def play():
+def get_computer_choice():
     choices = ["Rock", "Paper", "Scissors"]
-
-    def get_computer_choice():
-        return rd.choice(choices)
+    return rd.choice(choices)
         
+def get_user_choice():
+    user_choice = get_prediction()
+    return user_choice
 
-    def get_user_choice():
-        user_choice = get_prediction()
-        return user_choice
-
-    cpu_choice = get_computer_choice()
-    user_choice = get_user_choice()
-
-    def get_winner():
+def play():
+    computer_wins = 0
+    user_wins = 0
+    round_number = 1
+    game_active = True
+    while game_active:
+        print(f"\n------------------------\nRound {round_number} >>> The Current Score is: You = {user_wins} CPU = {computer_wins} ")
+        cpu_choice = get_computer_choice()
+        user_choice = get_user_choice()
 
         winning_combos = {
             "Rock": "Scissors",
@@ -77,14 +77,44 @@ get_prediction()
         }
 
         if user_choice == cpu_choice:
-            print(f"CPU plays {cpu_choice}. It's a tie!")
+            print(f"\nCPU plays {cpu_choice}. It's a tie!")
+            round_number += 1
         elif winning_combos[user_choice] == cpu_choice:
-            print(f"CPU plays {cpu_choice}. You won!")
+            print(f"\nCPU plays {cpu_choice}. You won!")
+            user_wins += 1
+            round_number += 1
         else:
-            print(f"CPU plays {cpu_choice}. You lost!")
-        
+            print(f"\nCPU plays {cpu_choice}. You lost!")
+            computer_wins += 1
+            round_number += 1
 
-    get_winner()
+        if user_wins == 3: 
+            print(f"""\n
+    Well done! You win!!!
 
+    Final Score:
 
-play()"""
+    User: {user_wins}
+
+    CPU : {computer_wins}""")
+            game_active = False
+
+        elif computer_wins == 3:
+            print(f"""\n
+    Sorry...You lose!!!
+
+    Final Score:
+
+    User: {user_wins}
+
+    CPU : {computer_wins}""")
+            game_active = False
+            
+    play_again = input("\nWould you like to play again? Y or N: ")
+
+    if play_again in ["y","Y","Yes", "yes"]:
+        play()
+    else:
+        print("\nThankyou for you playing!")
+
+play()
--- manual_rps.py
+++ manual_rps.py
@@ -1,21 +1,22 @@
 import random as rd
 
+def get_computer_choice():
+    choices = ["Rock", "Paper", "Scissors"]
+    return rd.choice(choices)
+        
+def get_user_choice():
+    user_choice = input("\nPlease choose between Rock, Paper or Scissors: ")
+    return user_choice
 
 def play():
-    choices = ["Rock", "Paper", "Scissors"]
-
-    def get_computer_choice():
-        return rd.choice(choices)
-        
-
-    def get_user_choice():
-        user_choice = input("Please choose between Rock, Paper or Scissors: ")
-        return user_choice
-
-    cpu_choice = get_computer_choice()
-    user_choice = get_user_choice()
-
-    def get_winner():
+    computer_wins = 0
+    user_wins = 0
+    round_number = 1
+    game_active = True
+    while game_active:
+        print(f"\n------------------------\nRound {round_number} >>> The Current Score is: You = {user_wins} CPU = {computer_wins} ")
+        cpu_choice = get_computer_choice()
+        user_choice = get_user_choice()
 
         winning_combos = {
             "Rock": "Scissors",
@@ -24,17 +25,44 @@ def play():
         }
 
         if user_choice == cpu_choice:
-            print(f"CPU plays {cpu_choice}. It's a tie!")
+            print(f"\nCPU plays {cpu_choice}. It's a tie!")
+            round_number += 1
         elif winning_combos[user_choice] == cpu_choice:
-            print(f"CPU plays {cpu_choice}. You won!")
+            print(f"\nCPU plays {cpu_choice}. You won!")
+            user_wins += 1
+            round_number += 1
         else:
-            print(f"CPU plays {cpu_choice}. You lost!")
-        
+            print(f"\nCPU plays {cpu_choice}. You lost!")
+            computer_wins += 1
+            round_number += 1
 
-    get_winner()
+        if user_wins == 3: 
+            print(f"""\n
+    Well done! You win!!!
 
+    Final Score:
+
+    User: {user_wins}
+
+    CPU : {computer_wins}""")
+            game_active = False
+
+        elif computer_wins == 3:
+            print(f"""\n
+    Sorry...You lose!!!
+
+    Final Score:
+
+    User: {user_wins}
+
+    CPU : {computer_wins}""")
+            game_active = False
+            
+    play_again = input("\nWould you like to play again? Y or N: ")
+
+    if play_again in ["\ny","Y","Yes", "yes"]:
+        play()
+    else:
+        print("\nThankyou for you playing!")
 
 play()
-    
-    
-    
